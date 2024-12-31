44287f1959db84c462a2c285545213f8fd5d7781
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2025-01-01 01:37:41 +0600

    Mongoose Default validation explore done

--- src/app/modules/student/student.schema.ts
+++ src/app/modules/student/student.schema.ts
@@ -7,7 +7,7 @@ import {
 } from './student.interface';
 
 const studentNameSchema = new Schema<studentName>({
-  firstName: { type: String, required: true },
+  firstName: { type: String, required: [true, 'First name is required'] },
   middleName: { type: String },
   lastName: { type: String, required: true },
 });
@@ -29,20 +29,50 @@ const LocalGuardianSchema = new Schema<LocalGuardian>({
 });
 
 const studentSchema = new Schema<Student>({
-  id: { type: String, required: true },
-  name: studentNameSchema,
-  gender: ['male', 'female'],
+  id: { type: String, required: true, unique: true },
+  name: {
+    type: studentNameSchema,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ['male', 'female'],
+      message: '{VALUE} is not a valid gender.',
+    },
+    required: true,
+  },
   dateOfBirth: { type: String, required: true },
   email: { type: String, required: true },
   contactNumber: { type: String, required: true },
   emergencyContactNo: { type: String, required: true },
-  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  bloodGroup: {
+    type: String,
+    enum: {
+      values: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+      message: '{VALUE} is not a valid blood group.',
+    },
+    required: true,
+  },
   presentAddress: { type: String, required: true },
   permanentAddress: { type: String, required: true },
-  guardian: GuardianSchema,
-  localGuardians: LocalGuardianSchema,
+  guardian: {
+    type: GuardianSchema,
+    required: true,
+  },
+  localGuardians: {
+    type: LocalGuardianSchema,
+    required: true,
+  },
   profileImg: { type: String },
-  isActive: ['active', 'inactive'],
+  isActive: {
+    type: String,
+    enum: {
+      values: ['active', 'inactive'],
+      message: '{VALUE} is not a valid status.',
+    },
+    required: true,
+  },
 });
 
 const StudentModel = model<Student>('Student', studentSchema);
